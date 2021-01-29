fc775c04c1bcf2989851260079636fafd776d219
Eduardo Chiamulera <eduardo.chiamulera@totvs.com.br>
2021-01-29 10:31:09 -0300

    criado projeto webApi e classe para injeção de dependencia

---
+++ CpmPedidos.API/Controllers/AppBaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.API.Controllers
+{
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider ServiceProvider;
+        public AppBaseController(IServiceProvider serviceProvider)
+        { 
+            ServiceProvider = serviceProvider;
+        }
+
+    }
+}
---
+++ CpmPedidos.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace CpmPedidos.API
+{
+    public class DependencyInjection
+    {
+        public static void Register(IServiceCollection serviceProvider)
+        {
+            RepositorDependence(serviceProvider);
+        }
+
+        private static void RepositorDependence(IServiceCollection serviceProvider)
+        {
+        }
+    }
+}
